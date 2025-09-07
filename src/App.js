1c621f29069ec581704c6f7dd8df549f49ae6e29
sukumarsahoo75-cmyk <sukumarsahoo75@gmail.com>
2025-09-07 22:30:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
           className={`transition-transform duration-1000 ease-out ${
             loaded ? "opacity-100" : "opacity-0"
           }`}
-          style={{ width: "400px", height: "auto", margin: "0", filter: "brightness(0) invert(1)" }}
+          style={{ width: "400px", height: "auto", margin: "0" }}
         />
 
         {/* Menu with minimal spacing */}
